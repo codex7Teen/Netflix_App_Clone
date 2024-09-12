df35e61f06a7d3331e238908ece23c09b4abca82
Dennizx <djconnect189@gmail.com>
2024-09-12 21:40:52 +0530

    appbar done

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,8 +5,34 @@ class ScreenHome extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('this is home screen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset(
+          'assets/logo.png',
+          height: 50,
+          width: 120,
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 20),
+            child: InkWell(onTap: () {
+            
+            }, child: Icon(Icons.search, color: Colors.white, size: 30,)),
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(6),
+            child: Container(
+              color: Colors.blue,
+              height: 27,
+              width: 27,
+            ),
+          ),
+          SizedBox(width: 20,)
+        ],
+      ),
+      body: Center(
+        child: Text('this is home screen'),
+      ),
     );
   }
 }
--- lib/widgets/bottom_nav.dart
+++ lib/widgets/bottom_nav.dart
@@ -9,37 +9,36 @@ class BottomNavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          bottomNavigationBar: Container(
-              color: Colors.black,
-              height: 70,
-              child: TabBar(
-                tabs: [
-                  Tab(
-                    icon: Icon(Icons.home),
-                    text: 'Home',
-                  ),
-                  Tab(
-                    icon: Icon(Icons.search),
-                    text: 'Search',
-                  ),
-                  Tab(
-                    icon: Icon(Icons.photo_library),
-                    text: 'New & Hot',
-                  ),
-                ],
-                indicatorColor: Colors.transparent,
-                labelColor: Colors.white,
-                unselectedLabelColor: Color(0xff999999)
-                ,
-              )),
-              body: TabBarView(children: [
-                ScreenHome(),
-                ScreenSearch(),
-                ScreenMore()
-              ]),
-        ),
-        );
+      length: 3,
+      child: Scaffold(
+        bottomNavigationBar: Container(
+            color: Colors.black,
+            height: 70,
+            child: TabBar(
+              tabs: [
+                Tab(
+                  icon: Icon(Icons.home),
+                  text: 'Home',
+                ),
+                Tab(
+                  icon: Icon(Icons.search),
+                  text: 'Search',
+                ),
+                Tab(
+                  icon: Icon(Icons.photo_library),
+                  text: 'New & Hot',
+                ),
+              ],
+              indicatorColor: Colors.transparent,
+              labelColor: Colors.white,
+              unselectedLabelColor: Color(0xff999999),
+            )),
+        body: TabBarView(children: [
+          ScreenHome(),
+          ScreenSearch(),
+          ScreenMore(),
+        ]),
+      ),
+    );
   }
 }
